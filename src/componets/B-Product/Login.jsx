c1d8a837019238848ca0bf60ed1db1aee3f65ba1
ziyaoddinshaikh95 <ziyaoddinshaikh95@gmail.com>
2023-09-02 00:37:55 +0530

    email commit to remote

--- src/componets/B-Product/Login.jsx
+++ src/componets/B-Product/Login.jsx
@@ -14,6 +14,7 @@ import { useNavigate } from "react-router-dom";
 import valid from "./valid";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, seterror] = useState({});
   const navigate = useNavigate();
